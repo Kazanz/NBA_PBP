f23ddcf27b33eed3f43f9baaf6d9c8ef6e5b6818
Zach Kazanski <kazanski.zachary@gmail.com>
2016-08-03 10:35:10 -0400

    Broken build: add substitution tracker

--- playbyplay.py
+++ playbyplay.py
@@ -58,6 +58,48 @@ def get_play_by_play(gameid):
     return data
 
 
+class SubstitutionTracker(object):
+    """Keeps track of minutes played in the play-by-play for each player."""
+    player_times = {}
+    players_in_game = {}
+
+    def is_substitution(self, play, quarter, time):
+        if re.findall('enters the game for', play):
+            player1, player2 = play.split(" enters the game for ")
+            self.players_in_game[player1] = {'quarter': quarter, 'time': time}
+            self._update_player_time(player2, quarter, time, entering=False)
+            del self.players_in_game[player2]
+
+    def update_time(self, player, time, stats):
+        pass
+
+    def _update_player_time(self, player, quarter, time, entering):
+        in_min, in_sec = map(int, self.players_in_game.split(':'))
+        out_min, out_sec = map(int, time.split(':'))
+        play_time = 0
+
+        if player not in self.player_times:  # Is a starter and first sub out.
+            play_time = (quarter - 1) * 12 if quarter > 1 else 0
+            play_time += int(out_min)
+        else:
+            quarter_diff = quarter - self.players_in_game[player]['quarter']
+            if quarter_diff >= 2:
+                play_time += (quarter_diff - 1) * 12
+            if quarter_diff >= 1:
+                play_time += in_min
+            if quarter_diff == 0:
+                play_time += in_min - out_min
+            else:
+                quarter_length_min = 12 if quarter <= 4 else 5
+                play_time += quarter_length_min - out_min
+            # Seconds adjustment.
+            play_time += 1 if 60 - out_sec + in_sec >= 60 else -1
+
+        player_time = self.player_times.setdefault(player, 0)
+        player_time += play_time
+        self.player_times[player] = play_time
+
+
 class PlayByPlayToBoxScoreWriter(object):
     """Create a running boxscore from play by play data and write it to a db."""
 
@@ -66,25 +108,41 @@ class PlayByPlayToBoxScoreWriter(object):
         self.gameid = gameid
         self.pbp_data = get_play_by_play(gameid)
         self.running_box_score = {}
+        self.subtracker = SubstitutionTracker()
 
-    #TODO: Still needs to track play time for each player.
     def execute(self):
         for play in self.pbp_data:
             play_stats = self.play_to_stats(play['play'])
-            if not play_stats:
-                print("No stat for: {}".format(play['play']))
-            else:
+            if play_stats:
                 team = play['team']
                 self.running_box_score.setdefault(team, {})
-                for player, stats in play_stats.items():
-                    if player not in self.running_box_score[team]:
-                        self.running_box_score[team][player] = stats
-                    else:
-                        for stat, amount in stats.items():
-                            self.running_box_score[team][player].setdefault(
-                                stat, amount)
-                            self.running_box_score[team][player][stat] += amount
+                self.update_player_stats(team, play, play_stats)
                 self.commit_to_db(play, self.running_box_score)
+
+    def play_to_stats(self, play):
+        for method in METHODS:
+            stats = method(play)
+            if stats:
+                return stats
+        print("No stat for: {}".format(play))
+
+    def update_player_stats(self, team, play, play_stats):
+        for player, stats in play_stats.items():
+            import pdb; pdb.set_trace();
+            stats['MIN'] = self.get_time(play, player, stats)
+            if player not in self.running_box_score[team]:
+                self.running_box_score[team][player] = stats
+            else:
+                self.update_running_box_score(team, player, stats)
+
+    def get_time(self, play, player, stats):
+        self.subtracker.is_substitution(play)
+        self.subtracker.update_times(play['time'])
+
+    def update_running_box_score(self, team, player, stats):
+        for stat, amount in stats.items():
+            self.running_box_score[team][player].setdefault(stat, amount)
+            self.running_box_score[team][player][stat] += amount
 
     def commit_to_db(self, play, box_score):
         """Write the box score to the database."""
@@ -107,13 +165,6 @@ class PlayByPlayToBoxScoreWriter(object):
                 player_stats['is_home'] = play['home']
                 stats[team].append(player_stats)
         return stats
-
-    def play_to_stats(self, play):
-        for method in METHODS:
-            stats = method(play)
-            if stats:
-                return stats
-
 
 if __name__ == '__main__':
     PlayByPlayToBoxScoreWriter(player_box_score_table, '400878160').execute()
